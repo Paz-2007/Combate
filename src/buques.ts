1d0322486be7142fbcd1a4522e2242969b2cacee
Paz-2007 <mariapazlucia2007@gmail.com>
2026-09-15 17:35:31 -0300

    agregar clase tanque

--- src/buques.ts
+++ src/buques.ts
@@ -1,4 +1,14 @@
-export class Buques {
-    constructor() {
+export class tanque {
+    id: number;
+    vida: number;
+
+    constructor(id: number) {
+        this.id = id;
+        this.vida =20;
+    }
+    disparar(): void {
+    }
+    recbirDisparo(): void {
+        this.vida -= 1;
     }
 }
