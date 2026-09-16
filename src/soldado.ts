10b28b8aa9d54d6ec330f7e927b8b634c454c39b
Paz-2007 <mariapazlucia2007@gmail.com>
2026-09-16 17:05:44 -0300

    verificar si esta vivo y test

--- src/soldado.ts
+++ src/soldado.ts
@@ -4,12 +4,20 @@ export class Soldado {
 
     constructor(nombre: string) {
         this._nombre = nombre;
-        this._vida = 1;
+        this._vida = 1; 
+        //se inicializa la vida del soldado en 1
     }
     disparar(objetivo: Soldado): void {
         objetivo.recibirDisparo();
+        //el soldado dispara al objetivo
     }
     recibirDisparo(): void {
         this._vida -= 1;
+        //se le resta 1 a la vida del soldado
+    }
+    estaVivo(soldado: Soldado): boolean {
+        return soldado._vida > 0;
+        //se verifica si el soldado esta vivo
     }
 }
+
--- tests/soldados.test.ts
+++ tests/soldados.test.ts
@@ -1,16 +1,21 @@
 import { describe, expect, test } from "vitest";
 
-import { Soldado } from "../src/Soldado.js";
+import { Soldado } from "../src/soldado.js";
 
-describe("Soldado", () => {
-    test("Cuando un soldado dispara a otro, el otro pierde una vida", () => {
-        const soldado1 = new Soldado("Soldado 1");
-        const soldado2 = new Soldado("Soldado 2");
-        //se crean dos soldados con nombre y vida inicial de 1
 
-        soldado1.disparar(soldado2);
-        //el soldado 1 dispara al soldado 2
-
-        expect(soldado2.vida).toBe(0);
+describe("Soldado"), () => {
+    test("Soldado mmuere cuando recibe un disparo", () => {
+        var soldado1 = new Soldado("Soldado 1");
+        //se crea un soldado con nombre y vida inicial de 1
+        var soldado2 = new Soldado("Soldado 2");
+        //se crea un soldado con nombre y vida inicial de 1
+        soldado2.disparar(soldado1);
+        //el soldado 2 dispara al soldado 1
+        soldado1.estaVivo(soldado1);
+        //se verifica si el soldado 1 esta vivo
+        expect(soldado1.estaVivo(soldado1)).toBe(false);
     });
-});
+}
+
+
+
